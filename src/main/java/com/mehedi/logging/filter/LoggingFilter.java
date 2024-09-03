ea6878b49957a6281e376d8de3cbc8a29d5eee84
Mehedi Hasan (Parvez) <hasanmehedi185@gmail.com>
2024-09-03 17:55:44 +0600

    Added unique ID for each request

--- src/main/java/com/mehedi/logging/filter/LoggingFilter.java
+++ src/main/java/com/mehedi/logging/filter/LoggingFilter.java
@@ -12,6 +12,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 @PropertySource("classpath:logging.properties")
@@ -31,17 +32,21 @@ public class LoggingFilter implements Filter {
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpRequest = (HttpServletRequest) request;
             String requestURI = httpRequest.getRequestURI();
+            String uniqueId = UUID.randomUUID().toString();
 
             String apiLogFileName = determineLogFileName(requestURI);
             MDC.put("apiLogFileName", apiLogFileName);
             MDC.put("apiEndpoint", requestURI);
+            MDC.put("uniqueId", uniqueId);
 
-            logger.info("Starting API call. HTTP Method: {}, URI: {}", httpRequest.getMethod(), requestURI);
+            logger.info("Starting API call. HTTP Method: {}, URI: {}, Unique ID: {}",
+                        httpRequest.getMethod(), requestURI, uniqueId);
 
             try {
                 chain.doFilter(request, response);
             } finally {
-                logger.info("Completed API call. HTTP Method: {}, URI: {}", httpRequest.getMethod(), requestURI);
+                logger.info("Completed API call. HTTP Method: {}, URI: {}, Unique ID: {}",
+                            httpRequest.getMethod(), requestURI, uniqueId);
                 MDC.clear();  // Clear the MDC after the request is processed
             }
         } else {
