478a0c02a90149fb85edd148a1329eb70e40d406
Mehedi Hasan (Parvez) <hasanmehedi185@gmail.com>
2024-09-03 16:42:45 +0600

    Changes for making properties based

--- src/main/java/com/mehedi/logging/filter/LoggingFilter.java
+++ src/main/java/com/mehedi/logging/filter/LoggingFilter.java
@@ -5,42 +5,46 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@PropertySource("classpath:logging.properties")
 //@Order(1)  // Order in which the filter will be executed; lower value indicates higher precedence
 public class LoggingFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
-    @Override
-    public void init(FilterConfig filterConfig) {
+    @Autowired
+    private Environment env;
 
-    }
+    @Override
+    public void init(FilterConfig filterConfig) { }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpRequest = (HttpServletRequest) request;
             String requestURI = httpRequest.getRequestURI();
-            String method = httpRequest.getMethod();
+//            String apiFileName = httpRequest.getRequestURI().replaceAll("[/{}]", "_").replaceAll("[^a-zA-Z0-9_]", "");
 
-            String apiFileName = httpRequest.getRequestURI();
-            apiFileName = apiFileName.replaceAll("[/{}]", "_").replaceAll("[^a-zA-Z0-9_]", "");
+            // Fetch the log file name from logging.properties, fall back to 'common' if not found
+            String apiLogFileName = env.getProperty("api.logging." + requestURI, "common");
 
+            MDC.put("apiLogFileName", apiLogFileName);
             MDC.put("apiEndpoint", requestURI);
-            MDC.put("httpMethod", method);
-            MDC.put("apiFileName", apiFileName);
 
-            logger.info("Starting API call. HTTP Method: {}, URI: {}", method, requestURI);
+            logger.info("Starting API call. HTTP Method: {}, URI: {}", httpRequest.getMethod(), requestURI);
 
             try {
                 chain.doFilter(request, response);
             } finally {
-                logger.info("Completed API call. HTTP Method: {}, URI: {}", method, requestURI);
+                logger.info("Completed API call. HTTP Method: {}, URI: {}", httpRequest.getMethod(), requestURI);
                 MDC.clear();  // Clear the MDC after the request is processed
             }
         } else {
@@ -49,7 +53,5 @@ public class LoggingFilter implements Filter {
     }
 
     @Override
-    public void destroy() {
-
-    }
+    public void destroy() { }
 }
